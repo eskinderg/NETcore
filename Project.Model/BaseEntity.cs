5a2be3483e4f41313671d7dfc17e2a998937a8ef
eskinderg <eskinderget@gmail.com>
2021-11-22 08:54:35 +0300

    formatting

--- Project.Model/BaseEntity.cs
+++ Project.Model/BaseEntity.cs
@@ -6,7 +6,7 @@ namespace Project.Model
   public abstract class BaseEntity
   {
     [Key]
-   [DatabaseGenerated(DatabaseGeneratedOption.Identity)] 
-    public int Id { get; set; }
+    [DatabaseGenerated(DatabaseGeneratedOption.Identity)] 
+      public int Id { get; set; }
   }
 }
